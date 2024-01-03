f6f1fec5f18b7c1e41f0a4c1dfaeecffd5e59863
Naseem Usmani <naseemusmani@me.com>
2024-01-03 21:35:17 +0000

    Refactor variable names for clarity

--- README.md
+++ README.md
@@ -1,6 +1,64 @@
+# Saiba CLI
+
+Saiba CLI is a command-line interface tool designed to streamline the creation and management of Saiba Web Projects, particularly focused on SvelteKit applications.
+
+## Features
+
+-   **Create SvelteKit Projects**: Quickly scaffold a new SvelteKit project with the latest features and best practices.
+
+## Getting Started
+
+### Prerequisites
+
+Before you begin, ensure you have the following installed on your system:
+
+-   Git
+-   Node.js
+-   Go
+
+### Installation
+
+To install the Saiba CLI, you can use the Go command:
+
 ```bash
 go install
+```
+
+Then build the executable. Feel free to change the .exe name.
+
+```bash
 go build -o saiba.exe main.go
 ```
 
-Then set to your $PATH variable
+## Adding Saiba CLI to Your PATH
+
+To use the saiba command from anywhere on your system, you need to add the directory containing the saiba.exe to your PATH.
+
+**For Windows:**
+Search for Environment Variables in your Start menu and select "Edit the system environment variables".
+In the System Properties window, click on the "Environment Variables" button.
+Under "System Variables", find and select the Path variable and click "Edit".
+Click "New" and add the path to the folder where saiba.exe is located.
+Click "OK" to save and close all the dialogs.
+
+**For Unix/Linux/macOS:**
+Add the following line to your ~/.bashrc, ~/.zshrc, or the appropriate configuration file for your shell:
+
+```bash
+export PATH=$PATH:/path/to/saiba
+Replace /path/to/saiba with the actual path to the saiba executable.
+```
+
+## Usage
+
+To create a new SvelteKit project, simply run:
+
+```bash
+saiba create
+```
+
+This will create a new SvelteKit project using the template from the official SvelteKit repository.
+
+## Contributing
+
+Contributions to Saiba CLI are welcome! Feel free to fork the repository, make your changes, and submit a pull request.
--- cmd/create.go
+++ cmd/create.go
@@ -2,8 +2,10 @@ package cmd
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
 
@@ -28,10 +30,14 @@ var createCmd = &cobra.Command{
 			case "SASS":
 				addSASS()
 			case "Lucia Auth":
-				fmt.Println("Lucia Auth")
+				addLuciaAuth()
 			case "Iconify Icons":
-				fmt.Println("Iconify Icons")
+				addIconifyIcons()
 			}
+		}
+
+		if tui.GetIncludeSaibaUI() {
+			addSaibaUI()
 		}
 
 	},
@@ -52,11 +58,7 @@ func createSvelteKit(projectName string) {
 }
 
 func addSASS() {
-	if err := os.Chdir(tui.GetProjectName()); err != nil {
-		fmt.Printf("Could not change directory: %v\n", err)
-	} else {
-		fmt.Printf("Changed directory to %s\n", tui.GetProjectName())
-	}
+	gotoDir(tui.GetProjectName())
 
 	cmd := exec.Command("npx", "svelte-add@latest", "sass")
 
@@ -69,6 +71,122 @@ func addSASS() {
 	} else {
 		fmt.Printf("SASS installed correctly!\n")
 	}
+
+	cloneAndCopySubdir("https://github.com/mastergrimm/saiba-cli.git", "templates/sass", "src/lib")
+}
+
+func addLuciaAuth() {
+	gotoDir(tui.GetProjectName())
+
+	cloneAndCopySubdir("https://github.com/mastergrimm/saiba-cli.git", "templates/lucia", "src/lib")
+}
+
+func addIconifyIcons() {
+	gotoDir(tui.GetProjectName())
+
+	cmd := exec.Command("npm", "install", "--save-dev", "@iconify/svelte")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	fmt.Println("Installing Iconify icons...")
+	if err := cmd.Run(); err != nil {
+		fmt.Printf("Failed to install Iconify icons: %v\n", err)
+	} else {
+		fmt.Println("Iconify icons installed successfully.")
+	}
+}
+
+func addSaibaUI() {
+	gotoDir(tui.GetProjectName())
+
+	cloneAndCopySubdir("https://github.com/mastergrimm/saiba-cli.git", "templates/saibaUI", "src/lib")
+}
+
+func gotoDir(dir string) {
+
+	if err := os.Chdir(dir); err != nil {
+		fmt.Printf("Could not change directory: %v\n", err)
+	} else {
+		fmt.Printf("Changed directory to %s\n", tui.GetProjectName())
+	}
+}
+
+func cloneAndCopySubdir(repo, subdir, destination string) error {
+	tempDir, err := os.MkdirTemp("", "repo-")
+	if err != nil {
+		return fmt.Errorf("failed to create temp directory: %w", err)
+	}
+	defer os.RemoveAll(tempDir)
+
+	cloneCmd := exec.Command("git", "clone", repo, tempDir)
+	if err := runCommand(cloneCmd); err != nil {
+		return fmt.Errorf("failed to clone repository: %w", err)
+	}
+
+	srcPath := filepath.Join(tempDir, subdir)
+	if err := copyDir(srcPath, destination); err != nil {
+		return fmt.Errorf("failed to copy subdirectory: %w", err)
+	}
+
+	return nil
+}
+
+func runCommand(cmd *exec.Cmd) error {
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+func copyDir(src, dst string) error {
+	var err error
+	var fds []os.DirEntry
+	var srcinfo os.FileInfo
+
+	if srcinfo, err = os.Stat(src); err != nil {
+		return err
+	}
+
+	if err = os.MkdirAll(dst, srcinfo.Mode()); err != nil {
+		return err
+	}
+
+	if fds, err = os.ReadDir(src); err != nil {
+		return err
+	}
+	for _, fd := range fds {
+		srcfp := filepath.Join(src, fd.Name())
+		dstfp := filepath.Join(dst, fd.Name())
+
+		if fd.IsDir() {
+			if err = copyDir(srcfp, dstfp); err != nil {
+				fmt.Println(err)
+			}
+		} else {
+			if err = copyFile(srcfp, dstfp); err != nil {
+				fmt.Println(err)
+			}
+		}
+	}
+	return nil
+}
+
+func copyFile(src, dst string) error {
+	in, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+
+	out, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	if _, err = io.Copy(out, in); err != nil {
+		return err
+	}
+	return out.Close()
 }
 
 func init() {
